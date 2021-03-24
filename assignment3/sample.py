4dab703cdf6185bf81e2238af15bff94c7e8331a
Amit32624 <120220153@umail.ucc.ie>
2021-03-24 20:42:53 +0000

    test.py file updated

--- assignment3/sample.py
+++ assignment3/sample.py
@@ -1,23 +1,10 @@
 from email.parser import Parser
 from email.policy import default
 
-# If the e-mail headers are in a file, uncomment these two lines:
-# with open("1_", 'rb') as fp:
-#     headers = BytesParser(policy=default).parse(fp)
-# headers = BytesParser(policy=default).parse(fp)
-#  Or for parsing headers in a string (this is an uncommon operation), use:
-# headers = Parser(policy=default).parsestr(
-#         'From: Foo Bar <user@example.com>\n'
-#         'To: <someone_else@example.com>\n'
-#         'Subject: Test message\n'
-#         '\n'
-#         'Body would go here\n')
-
-#  Now the header items can be accessed as a dictionary:
 red_flags = {"lay-k": ["kenneth.lay@enron.com", "klay@enron.com"],
 "skilling-j": ["skilling@enron.com", "jeff.skilling@enron.com"]}
 import os
-for root, dirs, files in os.walk('enron_email_corpus_2'):
+for root, dirs, files in os.walk('zufferli-j'):
     
     for file in files:
         # print(root)
@@ -25,6 +12,7 @@ for root, dirs, files in os.walk('enron_email_corpus_2'):
         with open(os.path.join(root, file), "r") as auto:
             headers = Parser().parse(auto)
             to =[]
+            # print("headers",headers.email.message.message)
             # print('To: %s' % headers['to'])
             # to_raw = (headers['to']).split()
             # to_new = [s.replace(',', '') for s in to_raw]
@@ -49,6 +37,8 @@ for root, dirs, files in os.walk('enron_email_corpus_2'):
                                             print('From: %s' % headers['from'])
                                             print('To:%s' % headers['To'])
                                             print('Subject: %s' % headers['subject'])
+                                            print("Email",headers.email.message.message)
+
                                             # print(headers.get_payload())
                                 except:
                                     if v in to_new:
@@ -64,33 +54,5 @@ for root, dirs, files in os.walk('enron_email_corpus_2'):
                 except:
                     pass
 
-            # print("to_new",to_new)
-            
-            # for k, v in red_flags.items():
-            #     try:
-            #         for i in v:
-            #             # print("v",v)
-            #             if i in to_new:
-            #                 # print("i",i)
-            #                 # print(k)
-            #                 print(i)
-            #                 print('Date: %s' % headers['Date'])
-            #                 print('From: %s' % headers['from'])
-            #                 print('To:',i)
-            #                 print('Subject: %s' % headers['subject'])
-                            
-            #     except:
-            #         if v in to_new:
-            #             # print("k_",k)
-            #                 print(i)
-            #                 print('Date: %s' % headers['Date'])
-            #                 print('From: %s' % headers['from'])
-            #                 print('To:',i)
-            #                 print('Subject: %s' % headers['subject'])
-
-            
-
-            # # You can also access the parts of the addresses:
-            # print('Recipient username: {}'.format(headers['to'].addresses[0].username))
-            # print('Sender name: {}'.format(headers['from'].addresses[0].username))
+ 
 
--- assignment3/test.py
+++ assignment3/test.py
@@ -1,41 +1,67 @@
-# import os
-# from email.parser import Parser
-# for root, dirs, files in os.walk('enron_email_corpus_1'):
-#      for file in files:
-#         print(root)
-#         print(dirs)
-#         print("files",file)
-# champ_ids = [0, 36, 85]
+from email.parser import Parser
+from email.policy import default
 
-# champ_dict = {"Bob" :[85,0] , "Carly": [22,45], "Freddy" : 85, "Megan" : 14, "Dilbert" : 69}
-# for k, v in champ_dict.items():
-#    try:
-#       for i in v:
-#          print("v",v)
-#          if i in champ_ids:
-#             print("i",i)
-#             print(k)
-#    except:
-#       if v in champ_ids:
-#          print("k_",k)
+red_flags = {"lay-k": ["kenneth.lay@enron.com", "klay@enron.com"],
+"skilling-j": ["skilling@enron.com", "jeff.skilling@enron.com"]}
+# red_flags = {"lay-k":"klay@enron.com",
+# "skilling-j": "jeff.skilling@enron.com"}
+import os
+for root, dirs, files in os.walk('enron_email_corpus'):
+    for file in files:
+        # print(root)
+        # print("files",file)
+        # print(dirs)
+        with open(os.path.join(root, file), "r") as auto:
+            headers = Parser().parse(auto)
+            to =[]
+            from_raw =(headers['from'])
+            from_ = from_raw
+            # print(from_)
+            for key, val in red_flags.items():
+                try:
+                    for i in val:
+                        if i ==from_:
+                            # print(i)
+                            # print("from",i)
+                            to_raw = (headers['to']).split()
+                            # print("to_raw",to_raw)
+                            to_new = [s.replace(',', '') for s in to_raw]
+                            # print(to_new)
+                            for k, v in red_flags.items():
+                                try:
+                                    for l in v:
+                                        if l in to_new:
+                                            # print(l)
+                                            # m =[]
+                                            m = (headers['to']).split()
+                                            m1 = [s.replace(',', '') for s in m]
+                                            # print(m1)
+                                            # print(len(m1))
+                                            if len(m1) <= 20 :
+                                                print(file)
+                                                print('Date: %s' % headers['Date'])
+                                                print('From: %s' % headers['from'])
+                                                # print('To:%s' % headers['To'])
+                                                print('To:',l)
+                                                print('Subject: %s' % headers['subject'])
+                                                print("Email",headers.email.message.message)
+                                            else:
+                                                pass
+                                        else:
+                                            if v in to_new:
+                                                print("Random",i)
+                                                print('Date: %s' % headers['Date'])
+                                                print('From: %s' % headers['from'])
+                                                print('To:%s' % headers['To'])
+                                                print('Subject: %s' % headers['subject'])
+                                            # print(headers.get_payload())
+                                except:
+                                    pass
+                                        
+                        else:
+                            pass
+                except:
+                    pass
 
+ 
 
-
-
-# red_flags = {"lay-k": ["kenneth.lay@enron.com", "klay@enron.com"],
-# "skilling-j": ["skilling@enron.com", "jeff.skilling@enron.com"]}
-
-# from_ = "jeff.skilling@enron.com"
-# for k, v in red_flags.items():
-#       try:
-#          for i in v:
-#             if i == from_:
-#                print("i",i)
-#             else:
-#                pass
-#       except:
-#             pass
-A=[]
-A = ['gmarmol@gmarmol.com,kenneth.lay@enron.com'] 
-to_new = [s.replace(',', '') for s in A]
-print(to_new)
